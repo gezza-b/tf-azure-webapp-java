091aae668563259ed06b68025fbf25b71617273d
Gerald <bachlmayr@gmail.com>
2018-11-16 14:42:49 +1100

    added resource group creation

--- main.tf
+++ main.tf
@@ -6,15 +6,16 @@ terraform {
   required_version = ">= 0.11.0"
 }
 
-resource "azurerm_resource_group" "webapp" {
-  name     = "${var.resource_group_name == "" ? replace(var.name, "/[^a-z0-9]/", "RG") : var.resource_group_name}"
+resource "azurerm_resource_group" "rg_webapp" {
+  name     = "${var.resource_group_name}"
   location = "${var.location}"
+  tags     = "${var.rg_tags}"
 }
 
 resource "azurerm_app_service_plan" "webserviceplan" {
   name                = "${var.service_plan_name == "" ? replace(var.name, "/[^a-z0-9]/", "") : var.service_plan_name}"
-  location            = "${azurerm_resource_group.webapp.location}"
-  resource_group_name = "${azurerm_resource_group.webapp.name}"
+  location            = "${azurerm_resource_group.rg_webapp.location}"
+  resource_group_name = "${azurerm_resource_group.rg_webapp.name}"
   
   kind                = "${var.plan_settings["kind"]}"
   
@@ -27,8 +28,8 @@ resource "azurerm_app_service_plan" "webserviceplan" {
 
 resource "azurerm_app_service" "webapp" {
   name                = "${var.name}"
-  location            = "${azurerm_resource_group.webapp.location}"
-  resource_group_name = "${azurerm_resource_group.webapp.name}"
+  location            = "${azurerm_resource_group.rg_webapp.location}"
+  resource_group_name = "${azurerm_resource_group.rg_webapp.name}"
   app_service_plan_id = "${azurerm_app_service_plan.webserviceplan.id}"
   site_config         = {
     java_container = "TOMCAT"
--- variables.tf
+++ variables.tf
@@ -1,20 +1,12 @@
-# ###############
-# required values
-# ###############
-
 variable "name" {
   description = "The name of the web app"
-  default = "mywebapp-test-0001"
+  default = "tfTomcat001"
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group in which the resources will be created."
-  default = "myrg"
+  default = "rg_webapp"
 }
-
-# ###############
-# optional values
-# ###############
 
 variable "location" {
   description = "Region where the resources are created."
@@ -41,4 +33,13 @@ variable "service_plan_name" {
 variable "app_settings" {
   description = "A key-value pair of App Settings"
   default     = {}
+}
+
+variable "rg_tags" {
+  type        = "map"
+  description = "A map of the tags to use on the resources that are deployed with this module."
+
+  default = {
+    environment = "DEV"
+  }
 }
